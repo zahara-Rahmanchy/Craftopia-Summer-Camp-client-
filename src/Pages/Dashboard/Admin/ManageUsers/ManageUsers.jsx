65ec095d80df6091c69c8d06342b138c27801385
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-06-08 17:50:03 +0600

    manage users buttons complete

--- src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -1,18 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import useAxios from "../../../../hooks/useAxios";
 import {useQuery} from "@tanstack/react-query";
+import Swal from "sweetalert2";
 const ManageUsers = () => {
   const [axiosSecure] = useAxios();
+  const [isAdmindisable, setAdminDisable] = useState(false);
+  const [userId, setUserId] = useState();
 
   const {data: users = [], refetch} = useQuery(["users"], async () => {
     const res = await axiosSecure.get("/users");
 
     return res.data;
   });
+  const handleAdmin = user => {
+    fetch(`http://localhost:5000/users/${user._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({role: "admin", clicked: true}),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("modified", data);
+        if (data.modifiedCount) {
+          refetch(user._id);
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
-  console.log("data from tanstack", users.image);
-  const {image} = users;
-  console.log(image);
+  // instructor
+  const handleInstructor = user => {
+    fetch(`http://localhost:5000/users/${user._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({role: "instructor", clicked: true}),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("modified", data);
+        if (data.modifiedCount) {
+          refetch(user._id);
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Instructor Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   return (
     <div className="bg-teal-100 h-full">
       <h2 className="text-center text-3xl py-9 text-teal-900 font-semibold">
@@ -51,10 +99,19 @@ const ManageUsers = () => {
                 <td>{user.email}</td>
                 <td className="capitalize">{user.role}</td>
                 <td>
-                  <button className="btn btn-ghost bg-orange-300  text-white me-3">
+                  <button
+                    className="btn btn-ghost bg-orange-300  text-white me-3 hover:bg-orange-400 "
+                    onClick={() => handleAdmin(user)}
+                    disabled={user?.clicked === true ? true : false}
+                  >
                     Admin
                   </button>
-                  <button className="btn btn-ghost bg-teal-400  text-white md:me-2 mt-2">
+                  <button
+                    className="btn btn-ghost bg-teal-400  text-white md:me-2 mt-2 hover:bg-teal-600"
+                    // disabled={isAdmindisable}
+                    onClick={() => handleInstructor(user)}
+                    disabled={user?.clicked === true ? true : false}
+                  >
                     Instructor
                   </button>
                 </td>
